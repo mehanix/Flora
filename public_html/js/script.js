81e1403fd80d736c8473e1a7dd90e5a5b2cc1d15
mehanix <nixikunn@gmail.com>
2020-03-15 22:14:32 +0200

    work on integrating node

--- public_html/js/script.js
+++ public_html/js/script.js
@@ -2,22 +2,28 @@
 var btn = document.getElementById("myBtn")
 
 var modalViewPlant = document.getElementById("viewPlant")
-var span = document.getElementsByClassName("modal-close")[0];
+var closeModal = document.getElementsByClassName("modal-close")[0];
 
 var btnAddPlant = document.getElementById("btnAddPlant");
 var modalAddPlant = document.getElementById("addPlant");
+var closeAddModal = document.getElementsByClassName("modal-close")[1];
 
 
 btn.onclick = function() {
     modalViewPlant.style.display = "block";
 }
 
-span.onclick = function() {
+closeModal.onclick = function() {
     modalViewPlant.style.display = "none";
 }
 
+closeAddModal.onclick = function() {
+    modalAddPlant.style.display = "none";
+
+}
+
 window.onclick = function(event) {
-    if (event.target == this.modalViewPlant) {
+    if (event.target == this.modalViewPlant || event.target == this.modalAddPlant) {
         this.modalViewPlant.style.display = "none";
     }
 
