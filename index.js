437bfa6566b632eb6b597cfe5698db54b265b779
Nicoleta Ciausu <nixikunn@gmail.com>
2020-03-21 22:00:04 +0200

    CRUD work. implemented read, update

--- index.js
+++ index.js
@@ -1,6 +1,94 @@
 const express = require('express')
 const app = express()
 const port = 3000
-
+const morgan = require('morgan')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const fs = require('fs')
+// Middleware
+app.use(morgan('tiny'))
+app.use(cors());
+app.use(bodyParser.json())
 app.use(express.static('public_html'))
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// CRUD API
+
+// Create
+app.post("/plants", (req, res)=> {
+
+    const plantsList = readJSONFile();
+    plantsList.push(req.newPlant); //TODO scrie functie pt asta
+    writeJSONFile(plantsList)
+})
+
+
+// Read One
+app.get("/plants/:id", (req, res) =>{
+
+    const plantsList = readJSONFile();
+    let found = 0;
+    for (index in plantsList)
+        if (plantsList[index].id 
+            == req.params.id) {
+            res.send(plantsList[index])
+            found = 1;
+            break;
+        }
+    if(found == 0 )
+        res.status(404).send({message:"Not found"})
+})
+
+
+// Read All
+app.get("/plants", (req,res) =>{
+
+    res.send(readJSONFile())
+})
+
+// Update
+app.post("/plants/:id", (req,res)=>{
+
+    const plantsList = readJSONFile();
+    console.log(req.body)
+    for (let index=0;index<plantsList.length;index++)
+        if(plantsList[index].id == req.params.id) {
+            plantsList[index] = req.body
+            break
+        }
+    writeJSONFile(plantsList);
+    res.redirect("/index.html")
+    
+})
+
+// Delete
+app.delete("/plants/:id", (req,res) => {
+    const plantsList = readJSONFile();
+    for (let index=0;index<plantsList.length;index++)
+        if(plant.id == req.params.id) {
+            plantslist.splice(index,index);
+            break
+        }
+        writeJSONFile(plantsList);   
+})
+
+
+function readJSONFile() {
+
+    return JSON.parse(fs.readFileSync("database.json"))["plants"];
+}
+
+function writeJSONFile(content) {
+
+    fs.writeFileSync("database.json",
+    JSON.stringify({plants: content},null,'\t'),
+    "utf-8",
+    err => {
+        if(err){
+            console.log(err)
+        }
+    });
+}
+
+
 app.listen(port, () => console.log(`Flora listening on port ${port}!`))
--- public_html/js/script.js
+++ public_html/js/script.js
@@ -1,17 +1,15 @@
 
-var btn = document.getElementById("myBtn")
 
 var modalViewPlant = document.getElementById("viewPlant")
 var closeModal = document.getElementsByClassName("modal-close")[0];
 
 var btnAddPlant = document.getElementById("btnAddPlant");
+
 var modalAddPlant = document.getElementById("addPlant");
 var closeAddModal = document.getElementsByClassName("modal-close")[1];
-
-
-btn.onclick = function() {
-    modalViewPlant.style.display = "block";
-}
+var plantView = document.getElementById("plant-view");
+var cardAddPlant = document.getElementById("cardAddPlant")
+var plants = [];
 
 closeModal.onclick = function() {
     modalViewPlant.style.display = "none";
@@ -29,9 +27,104 @@ window.onclick = function(event) {
 
 }
 
+
+window.onload = getPlants()
+
+//AJAX. get all plants
+function getPlants(){
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("GET","/plants",true)
+    xhttp.send();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            plants = JSON.parse(this.responseText)
+            plants.forEach((plant) => {
+                plantView.innerHTML += ` <div id="${plant.id}" class="card" onclick="showViewModal(${plant.id})">
+                    <img src="${plant.img}" alt="Avatar" style="width:100%">
+                    <div class="container">
+                        <h4><b>${plant.name}</b></h4>
+                        <p>${plant.desc}</p>
+                    </div>`;
+
+            })
+            
+        }
+}
+}
+
 function showAddModal() {
     modalAddPlant.style.display="block";
 
+
 }
 
-//todo add close
+function showEditControls(plant) {
+    modalViewPlant.innerHTML = ` <div class="modal-content">
+    <div class="modal-plant-img">
+        <img src="${plant.img}">
+    </div>
+    <div class="modal-plant-info">
+        <form  method="POST" action="/plants/${plant.id}">
+            <label for="plant-name">Plant Name:</label>
+            <input id="plant-name"  name="name" type="text" value="${plant.name}"><br>
+            <input type="hidden" name="id" value="${plant.id}">
+            <p>Water every <input name="waterEvery" id="waterEvery" type="number" min="1" max="100" value="${plant.waterEvery}"> days</p>
+            <label for="plantImage">Image Link: </label>
+            <input id="plantImage"  name="img" type="text" value="${plant.img}"><br>
+            <p>Date last watered: <input name="lastWatered" type="date" id="lastWatered" name="lastWatered" value="${plant.lastWatered}"> </p>
+            <label for="plant-desc">Plant Description:</label>
+            <textarea  name="desc" id="plant-desc">${plant.desc}</textarea>
+            <br><br>
+            <button type="submit" class="modal-save-btn">
+                <i class="fa fa-save"></i> Save
+            </button>
+        </form>
+    </div>
+    <span class="modal-close">&times;</span>
+    <div class="modal-controls">
+            <div class="modal-button-edit">
+                <i class="fas fa-edit"></i>
+            </div>
+            <div class="modal-button-delete">
+                <i class="fas fa-trash-alt"></i>
+            </div>
+        </div>
+</div>`
+   
+
+}
+
+
+function showViewModal(id) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("GET","/plants/"+id,true)
+    xhttp.send();
+    xhttp.onreadystatechange = function() {
+        if(this.readyState==4 && this.status == 200){
+            var plant = JSON.parse(this.responseText)
+            modalViewPlant.innerHTML = ` <div class="modal-content">
+            <div class="modal-plant-img">
+                <img src="${plant.img}">
+            </div>
+            <div class="modal-plant-info">
+                <h1>${plant.name}</h1>
+                <p> Water every ${plant.waterEvery} days </p>
+                <p>Date last watered: ${plant.lastWatered}</p>
+            </div>
+            <span class="modal-close">&times;</span>
+    
+            <div class="modal-controls">
+                <div class="modal-button-edit" onclick='showEditControls(${JSON.stringify(plant)})'>
+                    <i class="fas fa-edit"></i>
+                </div>
+                <div class="modal-button-delete">
+                    <i class="fas fa-trash-alt"></i>
+                </div>
+            </div>
+            `
+                }
+    }
+    modalViewPlant.style.display="block";
+
+}
+
